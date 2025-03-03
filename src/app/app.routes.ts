e938562e3d2b98620d48e66bf1145e7b405718fd
Adham-Gamal-Abdelnasser <aboeladahem694@gmail.com>
2025-03-03 14:28:44 +0200

    404 page & routing set up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent, title: "Guide | Login"},
+    {path: '**', component: NotfoundComponent, title: "Guide | page not found"},
+
+];
  
