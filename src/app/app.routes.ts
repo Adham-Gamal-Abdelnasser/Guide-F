c7b555927844c04446b7b15954be5d346eef961c
Adham-Gamal-Abdelnasser <aboeladahem694@gmail.com>
2025-03-14 13:48:44 +0200

    courses component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { LoginComponent } from './login/login.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { DoctorprofileComponent } from './doctorprofile/doctorprofile.component';
 import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
 
 export const routes: Routes = [
 
-    {path: '', redirectTo: 'profile', pathMatch: 'full', title: "Guide | profile"},
+    {path: '', redirectTo: 'home', pathMatch: 'full', title: "Guide | profile"},
     {path: 'login', component: LoginComponent, title: "Guide | Login"},
     {path: 'home', component: HomeComponent, title: "Guide | home" , children:[
         {path: 'profile', component: DoctorprofileComponent, title: "Guide | doctor profile"},
+        {path: 'courses', component: CoursesComponent, title: "Guide | doctor courses"},
         
     ]},
     {path: '**', component: NotfoundComponent, title: "Guide | page not found"},
---
+++ src/app/coursestudents/coursestudents.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coursestudents',
+  standalone: true,
+  imports: [],
+  templateUrl: './coursestudents.component.html',
+  styleUrl: './coursestudents.component.css'
+})
+export class CoursestudentsComponent {
+
+}
