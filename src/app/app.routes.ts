a490724344e2a69724b2b00dbf032817ea3aca04
Adham-Gamal-Abdelnasser <aboeladahem694@gmail.com>
2025-03-14 13:59:31 +0200

    course \students

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { NotfoundComponent } from './notfound/notfound.component';
 import { DoctorprofileComponent } from './doctorprofile/doctorprofile.component';
 import { HomeComponent } from './home/home.component';
 import { CoursesComponent } from './courses/courses.component';
+import { CoursestudentsComponent } from './coursestudents/coursestudents.component';
 
 export const routes: Routes = [
 
@@ -12,6 +13,7 @@ export const routes: Routes = [
     {path: 'home', component: HomeComponent, title: "Guide | home" , children:[
         {path: 'profile', component: DoctorprofileComponent, title: "Guide | doctor profile"},
         {path: 'courses', component: CoursesComponent, title: "Guide | doctor courses"},
+        {path: 'coursestudents', component: CoursestudentsComponent, title: "Guide | doctor courses"},
         
     ]},
     {path: '**', component: NotfoundComponent, title: "Guide | page not found"},
