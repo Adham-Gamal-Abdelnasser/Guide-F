d26b4e631b337a4f40cbc3f47fe645f2e8e71659
Adham-Gamal-Abdelnasser <aboeladahem694@gmail.com>
2025-04-21 15:18:39 +0200

    project reinit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+     "./src/styles.scss"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
