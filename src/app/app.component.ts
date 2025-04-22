ad7439cc6598a77ccabd46ec413c67e467ae1044
Adham-Gamal-Abdelnasser <aboeladahem694@gmail.com>
2025-04-22 12:38:26 +0200

    forget password design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./components/login/login.component";
 import { RegisterComponent } from "./components/register/register.component";
+import { ForgetpassComponent } from "./components/forgetpass/forgetpass.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, RegisterComponent],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent, ForgetpassComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
